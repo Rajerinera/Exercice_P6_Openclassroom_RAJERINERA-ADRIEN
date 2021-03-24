357a190ae876fe0f77408aaad26813a29456a464
Rajerinera <adrien.rajerinera@gmail.com>
2021-03-24 17:13:17 +0100

    Fin des routes, faut corriger l'authentification qui ne fonctionne pas

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Things');
-
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://adrienP6:abcdefgh@cluster0.mdvgq.mongodb.net/Cluster0?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -23,43 +22,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-    .then(() => res.status(201).json({message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({error}));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id},{...req.body, _id: req.params.id})
-    .then(()=> res.status(200).json({message: "L'objet a reçu une modif"}))
-    .catch(error => res.status(404).json({ error }));
-    });
-
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id})
-    .then(()=> res.status(200).json({message: "L'objet est supprimégi"}))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.use((req, res, next) => {
-    console.log('Le stuff mis en place');
-    next();
-});
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({error}));
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
